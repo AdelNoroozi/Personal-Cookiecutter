5410c9094673ded61c30d374f7671d357dd9d6f8
adel13no@gmail.com <adel13no@gmail.com>
2023-11-26 10:45:25 +0330

    base user manager added

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/users/managers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import BaseUserManager as BUM
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+
+        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            raise ValueError(_("Users must have a password"))
+
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,50 +1,16 @@
 from django.db import models
-from {{cookiecutter.project_slug}}.common.models import BaseModel
+from {{cookiecutter.project_slug}}.common.models import TimeStampedBaseModel
 
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import BaseUserManager as BUM
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 
+from {{cookiecutter.project_slug}}.users.managers import BaseUserManager
 
-
-class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
-
-        if password is not None:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-
-        user.full_clean()
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(
-            email=email,
-            is_active=True,
-            is_admin=True,
-            password=password,
-        )
-
-        user.is_superuser = True
-        user.save(using=self._db)
-
-        return user
-
-
-class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+class BaseUser(TimeStampedBaseModel, AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(verbose_name = _("email"),
                               unique=True)
-
-    is_active = models.BooleanField(default=True, verbose_name=_("is_active"))
     is_admin = models.BooleanField(default=False, verbose_name=_("is_admin"))
 
     objects = BaseUserManager()
